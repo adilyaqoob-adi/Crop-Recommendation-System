acf7032407eb63f8ac7fad649f0381fcc49e024e
Adil Yaqub <adil.yaqoob@nutech.edu.pk>
2026-01-28 22:37:11 +0500

    Update App.py

--- App.py
+++ App.py
@@ -9,7 +9,7 @@ col1, col2, col3 = st.columns([1, 3, 1])
 
 with col1:
     # Use 'use_container_width=True' to make it responsive
-    st.image("logo.png", width=120) 
+    st.image("logo.png", width=150) 
 
 with col2:
     # Center the title text horizontally
@@ -17,7 +17,7 @@ with col2:
     st.markdown("<h1 style='text-align: center;'>🌱 Precision Agriculture: Crop Recommender</h1>", unsafe_allow_html=True)
 
 with col3:
-    st.image("Active_logo.png", width=120)
+    st.image("Active_logo.png", width=150)
 st.markdown("---") # Adds a divider line below the header
 
 # 1. Load your saved model
@@ -122,6 +122,7 @@ st.sidebar.info("""
 Wheat and Sugarcane recommendations are coming in version 2.0!
 
 """)
+
 
 
 
